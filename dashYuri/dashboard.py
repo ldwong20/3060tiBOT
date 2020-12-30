d542d655e828455bd304bc98ad27a333063214d7
Lance Wong <ldwong20@gmail.com>
2020-12-30 18:03:26 -0500

    added matplotlib
    added plotting for prices

--- dashYuri/dashboard.py
+++ dashYuri/dashboard.py
@@ -1,5 +1,6 @@
 import time
 import datetime
+import matplotlib.pyplot as plt
 from BotYuri.search import openPage, searchPage
 
 prices = {}
@@ -20,9 +21,16 @@ def checkPrice(link, amount):
     print(prices)
 
 
+
+
 while True:
     checkPrice('https://www.amazon.com/HIFIMAN-SUNDARA-Over-Ear-Full-Size-Headphones/dp/B077XDWT7X/ref=sr_1_3?crid'
                '=2XSA51A1WO3MU&dchild=1&keywords=hifiman+sundara&qid=1609353646&sprefix=hifiman%2Caps%2C462&sr=8-3',
                300.0)
+
+    x, y = zip(*sorted(prices.items()))
+    plt.plot(x, y)
+    plt.show()
+
 
     time.sleep(5)
